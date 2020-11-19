84f7a85a9b4c76e4c120235717163be13f3c76a3
Ihar Leshchanka <igor.leshchenko@gmail.com>
2020-11-19 16:16:02 +0300

    complete TaskA2

--- src/by/it/leshchenko/jd01_05/TaskA.java
+++ src/by/it/leshchenko/jd01_05/TaskA.java
@@ -4,12 +4,30 @@ import static java.lang.Math.*;
 
 public class TaskA {
     public static void main(String[] args) {
+        TaskA1();
+        TaskA2();
+    }
+
+    static void TaskA1() {
         double a = 756.13;
         double x = 0.3;
         double part1 = cos(pow(x * x + PI / 6, 5));
         double part2 = sqrt(x * pow(a, 3));
         double part3 = log((a - 1.12 * x) / 4);
         double z = part1 - part2 - part3;
+//        double z = cos(pow(x * x + PI / 6, 5)) - sqrt(x * pow(a, 3)) - log((a - 1.12 * x) / 4);
+        System.out.println(z);
+    }
+
+    static void TaskA2() {
+        double a = 1.21;
+        double b = 0.371;
+        double part1 = tan(pow(a + b, 2));
+        double part2 = cbrt(a + 1.5);
+        double part3 = a * pow(b, 5);
+        double part4 = b / log(a * a);
+        double z = part1 - part2 + part3 - part4;
+//        double z = tan(pow(a + b, 2)) - cbrt(a + 1.5) + a * pow(b, 5) - b / log(a * a);
         System.out.println(z);
     }
 }
