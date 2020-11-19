dafa7fbfd24ab15eacc62d5664d04ca555c06cbe
tetenkovg <43035829+tetenkovg@users.noreply.github.com>
2020-11-19 20:32:15 +0300

    Task B2

---
+++ src/by/it/tetenkov/jd01_05/TaskB.java
@@ -0,0 +1,45 @@
+package by.it.tetenkov.jd01_05;
+import static java.lang.Math.*;
+public class TaskB {
+    public static void main(String[] args) {
+        stepone();steptwo();
+
+    }
+
+    private static void steptwo() {
+
+        double x = -6.0;
+        double a = 0;
+
+        for (x = -5.5; x < 2; x += 0.5) {
+            if (-2 < x / 2 && x / 2 < -1) {
+                a = log10(abs(sin(pow(x, 2)) + 2.74));
+                System.out.printf("При x/2=%f a=%f\n", x / 2, a);
+            } else if (-1 < x / 2 && x / 2 < 0.2) {
+                a = log10(abs(cos(pow(x, 2)) + 2.74));
+                System.out.printf("При x/2=%f a=%f\n", x / 2, a);
+            } else if (x / 2 == 2) {
+                a = log10(abs(1 / tan(pow(x, 2)) + 2.74));
+                System.out.printf("При x/2=%f a=%f\n", x / 2, a);
+            } else {
+                System.out.printf("При x/2=%f вычисления не определены\n", x / 2);
+            }
+
+        }
+    }
+
+    private static void stepone() {
+        double a = 0;
+        int x = 1;
+        double y = 0;
+
+        for (x = 1; x < 7; x++) {
+
+
+
+
+
+        }
+
+    }
+}
