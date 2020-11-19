562a2fe884ffaa8c027356a0e5528af4f58752c8
tetenkovg <43035829+tetenkovg@users.noreply.github.com>
2020-11-19 13:39:49 +0300

    I've 2nd and 3rd TaskA's parts

--- src/by/it/tetenkov/jd01_05/TaskA.java
+++ src/by/it/tetenkov/jd01_05/TaskA.java
@@ -4,10 +4,43 @@ import static java.lang.Math.*;
 
 public class TaskA {
     public static void main(String[] args) {
-        step();
+        stepone();
+        steptwo();
+        stepthree();
     }
 
-    private static void step() {
+    private static void stepthree() {
+        double x = 12.1;
+        double a = -5.0;
+
+        for (int i = -5; i <= 12 ; i++) {
+            double part1 = pow(E, a * x);
+            double part2 = 3.45 * a;
+            double f = part1 = part2;
+            System.out.printf("For x=%f a=%f f=%f", x, a, f);
+            a += 3.75;
+
+        }
+
+
+    }
+
+    private static void steptwo() {
+
+        double a = 1.21, b = 0.371;
+
+        double part1 = tan((pow(a,2)) + (2 * a * b) + (pow(b,2)));
+        double part2 = cbrt(a + 1.5);
+        double part4 = a * (pow(b, 5));
+        double part3 = b / ((log(pow(a, 2))));
+
+        double y = part1 - part2 + part4 - part3;
+        System.out.printf("For a=%f b=%f y=%f", a, b, y);
+        System.out.println();
+
+    }
+
+    private static void stepone() {
         double a = 756.13;
         double x = 0.3;
 
