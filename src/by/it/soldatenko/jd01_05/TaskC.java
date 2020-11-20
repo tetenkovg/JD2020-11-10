370608f6558360680aebdfc7f8850f25c1bb9ff6
ivansoldatenko <ivansoldatenko80@gmail.com>
2020-11-20 22:26:09 +0300

    i made home work

--- src/by/it/soldatenko/jd01_05/TaskC.java
+++ src/by/it/soldatenko/jd01_05/TaskC.java
@@ -1,23 +1,12 @@
 package by.it.soldatenko.jd01_05;
 
 
-
-import java.lang.*;
-import java.util.Arrays;
-
 import static java.lang.Math.*;
 
 public class TaskC {
     public static void main(String[] args) {
         Step1();
-
-
-
-
-
-
-
-
+        Step2();
     }
 
     private static void Step1() {
@@ -31,8 +20,6 @@ public class TaskC {
             x = (int) (x * 10000 + 0.5);
             x = x / 10000;
             arr[count] = pow((x * x + 4.5), value);
-//            System.out.println(x);
-//            System.out.println(count);
             x = x + deltaX;
             count++;
         }
@@ -72,6 +59,79 @@ public class TaskC {
                 System.out.println();
             }
         }
+    }
+    private static void Step2() {
+        int[] arr = new int[31];
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {//инициализация А
+            arr[i] = (int) (round(random() * 347) + 103);
+            if (arr[i] * 0.1 > i) {
+                count = count + 1;
+            }
+        }
+        int[] arr_10 = new int[count];// Инициализация В
+        count = 0;
+        for (int i = 0; i < 31; i++) {
+            if (arr[i] * 0.1 > i) {
+                arr_10[count] = arr[i];
+                count = count + 1;
+            }
+        }
+
+        int n = arr_10.length; // сортировка В
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++)
+                if (arr_10[j] > arr_10[j + 1]) {
+                    int temp = arr_10[j];
+                    arr_10[j] = arr_10[j + 1];
+                    arr_10[j + 1] = temp;
+                }
+        }
+        //печать А
+        System.out.println("Massiv A (index to rows)");
+        System.out.print("╔"+"══════════"+"╦"+"══════════"+"╦"+"══════════"+"╦"+"══════════"+"╦"+"══════════"+"╗");
+        System.out.println();
+        for (int i = 0; i < 35; i++) {
+            String name = "A";
+            if(i<arr.length) {
+                System.out.printf("║%s[%2d]=%-4d", name, i, arr[i]);
+            }else System.out.print("║          ");
+
+            if ((i + 1) % 5 == 0 || i + 1 == 35) {
+                System.out.println("║");
+                if (i <34) {
+                    System.out.println("╠" + "══════════" + "╬" + "══════════" + "╬" + "══════════" + "╬" + "══════════" + "╬" + "══════════" + "╣");
+                }
+            }
+
+        }
+        System.out.println("╚"+"══════════"+"╩"+"══════════"+"╩"+"══════════"+"╩"+"══════════"+"╩"+"══════════"+"╝");
+
+
+        // печать В
+        System.out.println("Massiv B (index to cols)");
+        String name = "B";
+        double d = arr_10.length;
+        int value = (int) ceil(d / 5);
+        System.out.print("╔"+"══════════"+"╦"+"══════════"+"╦"+"══════════"+"╦"+"══════════"+"╦"+"══════════"+"╗");
+        System.out.println();
+        for (int i = 0; i < value; i++) {
+            for (int k = 0; k < 25; k = k + 5) {
+                if (i + k < arr_10.length) {
+                    System.out.printf("║%s[%2d]=%-4d", name, i + k, arr_10[i + k]);
+                } else {
+                    System.out.print("║          ");
+                    break;
+                }
+            }
+            System.out.print("║");
+            System.out.println();
+            if (i<value-1) {
+                System.out.println("╠" + "══════════" + "╬" + "══════════" + "╬" + "══════════" + "╬" + "══════════" + "╬" + "══════════" + "╣");
+            }
+        }
+        System.out.println("╚"+"══════════"+"╩"+"══════════"+"╩"+"══════════"+"╩"+"══════════"+"╩"+"══════════"+"╝");
+
     }
 
 
