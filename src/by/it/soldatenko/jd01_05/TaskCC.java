742659a887b6e378151447734714c916f65f172f
ivansoldatenko <ivansoldatenko80@gmail.com>
2020-11-20 13:55:28 +0300

    i made home work

--- src/by/it/soldatenko/jd01_05/TaskCC.java
+++ src/by/it/soldatenko/jd01_05/TaskCC.java
@@ -10,26 +10,23 @@ public class TaskCC {
     private static void Step2() {
         int[] arr = new int[31];
         int count = 0;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {//инициализация А
             arr[i] = (int) (round(random() * 347) + 103);
             if (arr[i] * 0.1 > i) {
 //                System.out.print(arr[i] + " ");
                 count = count + 1;
             }
         }
-        System.out.println(count);
-        int[] arr_10 = new int[count];
+        int[] arr_10 = new int[count];// Инициализация В
         count = 0;
         for (int i = 0; i < 31; i++) {
             if (arr[i] * 0.1 > i) {
                 arr_10[count] = arr[i];
-
-//                System.out.print(arr_10[count] + " ");
                 count = count + 1;
             }
         }
 
-        int n = arr_10.length; // сортировка
+        int n = arr_10.length; // сортировка В
         for (int i = 0; i < n - 1; i++) {
             for (int j = 0; j < n - i - 1; j++)
                 if (arr_10[j] > arr_10[j + 1]) {
@@ -38,27 +35,48 @@ public class TaskCC {
                     arr_10[j + 1] = temp;
                 }
         }
-
-        for (int i = 0; i < 31; i++) {//печать А
+        //печать А
+        System.out.print("╔"+"══════════"+"╦"+"══════════"+"╦"+"══════════"+"╦"+"══════════"+"╦"+"══════════"+"╗");
+        System.out.println();
+        for (int i = 0; i < 35; i++) {
             String name = "A";
-            System.out.printf("%s[%2d]=%-8d", name, i, arr[i]);
-            if ((i + 1) % 5 == 0 || i + 1 == 31) {
-                System.out.println();
+            if(i<arr.length) {
+                System.out.printf("║%s[%2d]=%-4d", name, i, arr[i]);
+            }else System.out.print("║          ");
+
+            if ((i + 1) % 5 == 0 || i + 1 == 35) {
+                System.out.println("║");
+                if (i <34) {
+                    System.out.println("╠" + "══════════" + "╬" + "══════════" + "╬" + "══════════" + "╬" + "══════════" + "╬" + "══════════" + "╣");
+                }
             }
+
         }
+        System.out.println("╚"+"══════════"+"╩"+"══════════"+"╩"+"══════════"+"╩"+"══════════"+"╩"+"══════════"+"╝");
+
+
         // печать В
         String name = "B";
         double d = arr_10.length;
         int value = (int) ceil(d / 5);
+        System.out.print("╔"+"══════════"+"╦"+"══════════"+"╦"+"══════════"+"╦"+"══════════"+"╦"+"══════════"+"╗");
+        System.out.println();
         for (int i = 0; i < value; i++) {
             for (int k = 0; k < 25; k = k + 5) {
                 if (i + k < arr_10.length) {
-                    System.out.printf("%s[%2d]=%-8d", name, i + k, arr_10[i + k]);
-                } else break;
+                    System.out.printf("║%s[%2d]=%-4d", name, i + k, arr_10[i + k]);
+                } else {
+                    System.out.print("║          ");
+                    break;
+                }
             }
+            System.out.print("║");
             System.out.println();
-
-
+            if (i<value-1) {
+                System.out.println("╠" + "══════════" + "╬" + "══════════" + "╬" + "══════════" + "╬" + "══════════" + "╬" + "══════════" + "╣");
+            }
         }
+        System.out.println("╚"+"══════════"+"╩"+"══════════"+"╩"+"══════════"+"╩"+"══════════"+"╩"+"══════════"+"╝");
+
     }
 }
