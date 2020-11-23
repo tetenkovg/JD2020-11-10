98bf90b2efffd6231e22883d8fac42f22dbe4c74
Ihar Leshchanka <igor.leshchenko@gmail.com>
2020-11-23 18:56:50 +0300

    jd01_06 TaskA2 complete

--- src/by/it/leshchenko/jd01_06/TaskA1.java
+++ src/by/it/leshchenko/jd01_06/TaskA1.java
@@ -1,7 +1,7 @@
 package by.it.leshchenko.jd01_06;
 
-import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TaskA1 {
     public static void main(String[] args) {
