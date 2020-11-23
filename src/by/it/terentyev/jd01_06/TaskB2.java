af987cd1f6cba896bd0881936f30cd7ac3c9a3ed
Teren <byvladimirt@gmail.com>
2020-11-23 20:06:51 +0300

    test2

--- src/by/it/terentyev/jd01_06/TaskB2.java
+++ src/by/it/terentyev/jd01_06/TaskB2.java
@@ -5,17 +5,21 @@ import java.util.regex.Pattern;
 
 public class TaskB2 {
     public static void main(String[] args) {
+        String[][] arrPoem = new String[32][2];
         String sb = new String(Poem.text);
-       sb = sb.replaceAll(" *[^а-яА-ЯёЁ \n]+ *"," ");
-       sb = sb.trim();
+        sb = sb.replaceAll(" *[^а-яА-ЯёЁ \n]+ *"," ");
+        sb = sb.trim();
         Pattern pattern = Pattern.compile(".{1,}\n");
         Matcher matcher = pattern.matcher(sb);
+        int i=0;
         while (matcher.find()){
-            int start=matcher.start();
-            int end =matcher.end();
-            System.out.println(matcher.group()+ "ok");
-
-
+            int col=matcher.end()-matcher.start();
+            arrPoem [i][1] = matcher.group();
+            arrPoem[i][0]= String.valueOf(col);
+            //System.out.println(col);
+            //System.out.println(matcher.group()+ "ok");
+            i++;
         }
+        System.out.println(arrPoem[1][0]+" "+arrPoem[1][1]);
     }
 }
