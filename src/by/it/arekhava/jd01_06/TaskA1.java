d4b2d272ec6890ee0816aa358b6daf672c3ba7cd
N <no5110059@gmail.com>
2020-11-23 04:03:18 +0300

    jd01_05 class work started

--- src/by/it/arekhava/jd01_06/TaskA1.java
+++ src/by/it/arekhava/jd01_06/TaskA1.java
@@ -3,7 +3,7 @@ package by.it.arekhava.jd01_06;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class TaskA {
+public class TaskA1 {
     public static void main(String[] args) {
         StringBuilder dynText = new StringBuilder(Poem.text);
         Pattern pattern=Pattern.compile("[а-яА-ЯёЁ]{4,}");
