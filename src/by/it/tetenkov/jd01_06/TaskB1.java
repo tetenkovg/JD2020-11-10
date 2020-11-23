a31e24af9b36537b4b2a71f1ba95984ecf876357
tetenkovg <43035829+tetenkovg@users.noreply.github.com>
2020-11-24 00:13:25 +0300

    jd01-06 TaskA2, TaskB2

---
+++ src/by/it/tetenkov/jd01_06/TaskB1.java
@@ -0,0 +1,59 @@
+package by.it.tetenkov.jd01_06;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.*;
+
+public class TaskB1 {
+
+
+    private static String[] words = {};
+    private static String consonantsRussian = "цкнгшщзхфвпрлджбтмсч"; // согланые
+    private static String[] vowelsWords = {};
+    private static String vowelsRussian = "аоуэиыеёяюй"; // гласные
+
+
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]{2,}");
+        Matcher matcher = pattern.matcher(Poem.text);
+        //String[] words = Poem.text.split(" ");
+        //System.out.println(Arrays.toString(firstSort(words)));
+        //firstSort(words);
+        //secondsSort(words);
+
+        while (matcher.find()) {
+            String word = matcher.group();
+
+            int last = words.length;
+            words = Arrays.copyOf(words, last + 1);
+            words[last] = word;
+
+        }
+
+        filter(words);
+
+
+    }
+
+    private static void filter(String[] words) {
+        for (int i = 0; i < words.length; i++) {
+            if (consonantsRussian.indexOf(Character.toLowerCase(words[i].charAt(0))) != -1 && vowelsRussian.indexOf(Character.toLowerCase(words[i].charAt(words[i].length() - 1))) != -1) {
+                String word = words[i];
+                int last = vowelsWords.length;
+                vowelsWords = Arrays.copyOf(vowelsWords, last + 1);
+                vowelsWords[last] = word;
+
+            }
+
+        }
+
+        for (int i = 0; i < vowelsWords.length; i++) {
+            System.out.print(vowelsWords[i]);
+            System.out.println();
+        }
+    }
+}
+
+
