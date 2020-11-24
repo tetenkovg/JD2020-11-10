15d241aa061cbf4354706eec9b2fa4d926780b74
N <no5110059@gmail.com>
2020-11-24 11:45:57 +0300

    jd01_05 class work started

--- src/by/it/arekhava/jd01_06/TaskA2.java
+++ src/by/it/arekhava/jd01_06/TaskA2.java
@@ -5,34 +5,39 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TaskA2 {
-    private static String[] w = {};
-    private static int[] count = {};
+    private static  String[] w = {};
+    private static int [] count = {};
 
-    private static int pos(String word) {
+    private static int pos (String word){
         for (int i = 0; i < w.length; i++) {
-            if (w[i].equals(word));
+            if(w[i].equals(word)){
+
                 return i;
+
+            }
+
         }
+
         return -1;
     }
-
     public static void main(String[] args) {
-
+        //StringBuilder dynText = new StringBuilder(Poem.text);
         Pattern pattern = Pattern.compile("[а-яА-ЯёЁ]+");
         Matcher matcher = pattern.matcher(Poem.text);
-        while (matcher.find()) {
-            String word=matcher.group();
-            int p=pos(word);
-            if(p>=0) {
+
+        while(matcher.find()){
+            String word = matcher.group();
+            int p = pos(word);
+
+            if(p >= 0){
                 count[p]++;
             }
-            else{
-                int last=w.length;
-
-                w= Arrays.copyOf(w,  last+1);
-                w[last]=word;
-                count=Arrays.copyOf(count, last+1);
-                count [last]=1;
+            else {
+                int last = w.length;
+                w = Arrays.copyOf(w, last+1);
+                w[last] = word;
+                count = Arrays.copyOf(count, last+1);
+                count[last] = 1;
             }
         }
         for (int i = 0; i < w.length; i++) {
