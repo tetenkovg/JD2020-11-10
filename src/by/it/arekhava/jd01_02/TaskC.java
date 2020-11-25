7ef8c7e196ccf3a31d424fb0d8ae7d5004f077c3
N <no5110059@gmail.com>
2020-11-25 14:46:57 +0300

    jd01_05 class work started

--- src/by/it/arekhava/jd01_02/TaskC.java
+++ src/by/it/arekhava/jd01_02/TaskC.java
@@ -4,8 +4,7 @@ import java.util.Scanner;
 
 public class TaskC {
 
-    public static int[][] step1(int n)
-    {
+    public static int[][] step1(int n) {
         int[][] arr = new int[n][n];
 
 
@@ -14,66 +13,79 @@ public class TaskC {
             boolean isN = false;
             boolean isNegativeN = false;
 
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < n; j++)
-                {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
 
-                    int randomNumber = (int)(Math.random()*(2*n+1))-n;
+                    int randomNumber = (int) (Math.random() * (2 * n + 1)) - n;
 
-                    if (n == randomNumber)
-                    {
+                    if (n == randomNumber) {
                         isN = true;
                     }
-                    if (-n == randomNumber)
-                    {
+                    if (-n == randomNumber) {
                         isNegativeN = true;
                     }
                     arr[i][j] = randomNumber;
                 }
             }
-            if (isN && isNegativeN)
-            {
+            if (isN && isNegativeN) {
                 break;
             }
         }
 
         return arr;
     }
-    public static void main(String[] args)
-    {
-        Scanner scan=new Scanner (System.in);
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
 
         int[][] arr = step1(n);
 
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    System.out.print(arr[i][j] + " ");
-                }
-                System.out.println();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
             }
-    }
-    public static void step2(int[][] arr) {
-
-
-        for (int i = 0; i < arr.length; i++) {
-            {
-
-                for (int j = 0; j < arr[i].length; j++) {
-                    int sum = 0;
-                    int counter = 0;
-
-                    if (arr[i][j] > 0) {
-                        sum += arr[i][j];
-                        counter++;
-                        if (counter == 2) {
-                            break;
-                        }
-                    }
-                }
-            }
+            System.out.println();
         }
     }
 
+    public static int step2(int[][] arr) {
+        int sum;
+        int totalSum = 0;
+        int count;
+        int index;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum = 0;
+            for (int j = 0; j < arr.length; j++) {   //перебираем строку массива
+                if (arr[i][j] >= 0) {                     //и находим первое положительное число
+                    count = 0;
+                    index = ++j;
+                    //проверяем есть ли второе положительное число
+                    while (index < arr.length) {
+                        if (arr[i][index] >= 0) {
+                            count++;
+                        }
+                        index++;
+                    }
+                /* если второе положительное число есть - запускаем цикл для счёта суммы до тех пор
+                  пока следующее число не будет положительным */
+                    if (count > 0) {
+                        while (j < arr.length) {
+                            if (arr[i][j] < 0) {
+                                sum += arr[i][j];
+                            } else {
+                                break;
+                            }
+                            j++;
+                        }
+                    }
+                    break;
+                }
+            }
+            System.out.println(sum);
+            totalSum += sum;
+        }
+        return totalSum;
+    }
 }
