b938f459723ea832cfbf52e064a79cced1528360
N <no5110059@gmail.com>
2020-11-25 20:09:42 +0300

    jd01_05 class work started

---
+++ src/by/it/arekhava/jd01_07/Runner.java
@@ -0,0 +1,14 @@
+package by.it.arekhava.jd01_07;
+
+import java.sql.SQLOutput;
+
+public class Runner {
+    public static void main(String[] args) {
+        Var v1=new Scalar(3.14);
+        Var v2= new Vector(new double[] {1,2,4});
+        Var v3=
+        System.out.println(v1);
+        System.out.println(v2);
+    }
+
+}
