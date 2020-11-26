72c81421dbf584a1f45ff1ca43043d30c189fe5c
ivansoldatenko <ivansoldatenko80@gmail.com>
2020-11-26 11:44:53 +0300

    home work

---
+++ src/by/it/soldatenko/jd01_08/Scalar.java
@@ -0,0 +1,21 @@
+package by.it.soldatenko.jd01_08;
+
+class Scalar extends Var {
+    private double value;
+
+    Scalar(double value) {
+        this.value = value;
+    }
+    Scalar(String strValue){
+        this.value = Double.parseDouble(strValue);
+    }
+    Scalar(Scalar otherScalar){
+        this.value = otherScalar.value;
+    }
+
+    @Override
+    public String toString() {
+
+        return Double.toString(value);
+    }
+}
---
+++ src/by/it/soldatenko/jd01_08/Var.java
@@ -0,0 +1,8 @@
+package by.it.soldatenko.jd01_08;
+
+abstract class Var {
+    @Override
+    public String toString() {
+        return "some abstract Var{}";
+    }
+}
