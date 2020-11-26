280c3ef2ff3687cac5b65bb82523c32322c77d4d
shepard <www.shepard1992@gmail.com>
2020-11-26 22:04:07 +0300

    Homework jd01_08 B started

--- src/by/it/evstratov/jd01_08/Vector.java
+++ src/by/it/evstratov/jd01_08/Vector.java
@@ -1,4 +1,4 @@
-package by.it.evstratov.jd01_07;
+package by.it.evstratov.jd01_08;
 
 
 import java.util.Arrays;
@@ -24,6 +24,26 @@ class Vector extends Var{
             value[i] = Double.parseDouble(arrayStrings[i].trim());
         }
 
+    }
+
+    @Override
+    public Var add(Var other) {
+        if (other instanceof Scalar){
+            double otherValue = ((Scalar) other).getValue();
+            double[] arr = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < arr.length; i++) {
+                arr[i]+=otherValue;
+            }
+            return new Vector(arr);
+        }
+        else if (other instanceof Vector){
+            double[] arr = Arrays.copyOf(value, value.length);
+            for (int i = 0; i < arr.length; i++) {
+                arr[i]+=((Vector) other).value[i];
+            }
+            return new Vector(arr);
+        }
+        return super.add(other);
     }
 
     @Override
