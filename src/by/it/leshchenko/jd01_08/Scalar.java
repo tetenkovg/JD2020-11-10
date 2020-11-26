faa41a3b3ffbcbdfeb77182da373f68a96eaa5ac
Ihar Leshchanka <igor.leshchenko@gmail.com>
2020-11-26 15:51:45 +0300

    jd01_08: homework tasks A1-A4 complete

--- src/by/it/leshchenko/jd01_08/Scalar.java
+++ src/by/it/leshchenko/jd01_08/Scalar.java
@@ -1,5 +1,7 @@
 package by.it.leshchenko.jd01_08;
 
+import java.util.Scanner;
+
 class Scalar extends Var {
     private final double value;
 
@@ -13,6 +15,46 @@ class Scalar extends Var {
 
     public Scalar(String strScalar) {
         this.value = Double.parseDouble(strScalar);
+    }
+
+    @Override
+    public Var add(Var other) {
+        if (other instanceof Scalar) {
+            double sum = this.value + ((Scalar) other).value;
+            return new Scalar(sum);
+        } else {
+            return other.add(this);
+        }
+    }
+
+    @Override
+    public Var sub(Var other) {
+        if (other instanceof Scalar) {
+            double sub = this.value - ((Scalar) other).value;
+            return new Scalar(sub);
+        } else {
+            return new Scalar(-1).mul(other).add(this);
+        }
+    }
+
+    @Override
+    public Var mul(Var other) {
+        if (other instanceof Scalar) {
+            double mul = this.value * ((Scalar) other).value;
+            return new Scalar(mul);
+        } else {
+            return other.add(this);
+        }
+    }
+
+    @Override
+    public Var div(Var other) {
+        if (other instanceof Scalar) {
+            double div = this.value / ((Scalar) other).value;
+            return new Scalar(div);
+        } else {
+            return super.div(other);
+        }
     }
 
     @Override
