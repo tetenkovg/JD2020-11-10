12d64bbe6f3f07bc4f8f1f6a5dfe8c6425267079
Konstantinchik <kgluschenko83@gmail.com>
2020-11-28 14:35:11 +0300

    task 9 start

---
+++ src/by/it/kglushchenko/jd01_09/ConsoleRunner.java
@@ -0,0 +1,10 @@
+package by.it.kglushchenko.jd01_09;
+
+import java.util.Scanner;
+
+public class ConsoleRunner {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        Scanner sc = new Scanner(System.in);
+    }
+}
---
+++ src/by/it/kglushchenko/jd01_09/Patterns.java
@@ -0,0 +1,5 @@
+package by.it.kglushchenko.jd01_09;
+
+public interface Patterns {
+    String SCALAR = "-?[0-9]+(\\.[0-9]+)?";
+}
