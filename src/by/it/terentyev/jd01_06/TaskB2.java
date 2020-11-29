7b199f0fb2823c518afe53c394c1eb77e05c73e7
Teren <byvladimirt@gmail.com>
2020-11-29 12:46:17 +0300

    Task 7 level A

--- src/by/it/terentyev/jd01_06/TaskB2.java
+++ src/by/it/terentyev/jd01_06/TaskB2.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 public class TaskB2 {
     public static void main(String[] args) {
-        String[][] arrPoem = new String[32][2];
+        //String[][] arrPoem = new String[32][2];
         String[] arrPoem1 = new String[32];
         String sb = new String(Poem.text);
         sb = sb.replaceAll(" *[^а-яА-ЯёЁ \n]+ *"," ");
@@ -16,12 +16,16 @@ public class TaskB2 {
         int i=0;
         while (matcher.find()){
             int col=matcher.end()-matcher.start();
-            arrPoem [i][1] = matcher.group();
-            arrPoem[i][0]= String.valueOf(col);
+            //arrPoem [i][1] = matcher.group();
+            //arrPoem[i][0]= String.valueOf(col);
             arrPoem1 [i] = matcher.group();
             i++;
         }
-        System.out.println(Arrays.toString(arrPoem1));
-       //System.out.print(Arrays.deepToString(arrPoem));
+        //System.out.println(Arrays.toString(arrPoem1));
+//        Arrays.sort(arrPoem);
+//        for (String[] s:arrPoem) {
+//            System.out.print(s);
+//        }
+       System.out.println(Arrays.deepToString(arrPoem1));
     }
 }
