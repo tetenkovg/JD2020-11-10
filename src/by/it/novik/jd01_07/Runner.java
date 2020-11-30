8ad4599b257d80dc85822fb54cc40ea05b0e481d
novikilya47 <novikilya-1998@mail.ru>
2020-12-01 00:28:45 +0300

    Jd01_07

---
+++ src/by/it/novik/jd01_07/Runner.java
@@ -0,0 +1,21 @@
+package by.it.novik.jd01_07;
+
+public class Runner {
+    public static void main(String[] args) {
+        Var s1=new Scalar(3.1415);
+        Var s2=new Scalar("3.1415");
+        Var s3=new Scalar(new Scalar(3.1415));
+
+        Var v1=new Vector(new double[]{1,2,4});
+
+        Var v3=new Vector("{1,2,4}");
+
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(v1);
+
+        System.out.println(v3);
+    }
+}
