c8a664f8ef122f70bc3630717173a9e4b0fb282c
novikilya47 <novikilya-1998@mail.ru>
2020-12-01 14:11:34 +0300

    Jd01_07. C1-C2

--- src/by/it/novik/jd01_07/Runner.java
+++ src/by/it/novik/jd01_07/Runner.java
@@ -7,15 +7,16 @@ public class Runner {
         Var s3=new Scalar(new Scalar(3.1415));
 
         Var v1=new Vector(new double[]{1,2,4});
-
         Var v3=new Vector("{1,2,4}");
+
+        Var m1=new Matrix(new double[][]{{1,2},{3,4}});
 
 
         System.out.println(s1);
         System.out.println(s2);
         System.out.println(s3);
         System.out.println(v1);
-
         System.out.println(v3);
+        System.out.println(m1);
     }
 }
