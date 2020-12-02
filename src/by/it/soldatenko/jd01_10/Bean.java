4c70b5855f038099eef9fcb9277c86a547b9af60
ivansoldatenko <ivansoldatenko80@gmail.com>
2020-12-02 19:03:19 +0300

    home work

--- src/by/it/soldatenko/jd01_10/Bean.java
+++ src/by/it/soldatenko/jd01_10/Bean.java
@@ -2,16 +2,16 @@ package by.it.soldatenko.jd01_10;
 
 public class Bean {
     @Param(a=2,b=5)
-    double sum(int a,int b){
+    static double sum(int a,int b){
         return a+b;
     }
     @Param(a=2,b=5)
-    double avg(int a,int b){
+    static double avg(int a,int b){
         return (a+b)/2.0;
     }
-    @SuppressWarnings("ManualMaxMinCalculator")
+    @Param(a=2,b=5)
     double max(int a,int b){
-        return a>b ? a:b;
+        return Math.max(a,b);
     }
     double min(int a,int b){
         return Math.min(a,b);
--- src/by/it/soldatenko/jd01_10/PrintMath.java
+++ src/by/it/soldatenko/jd01_10/PrintMath.java
@@ -1,20 +1,41 @@
 package by.it.soldatenko.jd01_10;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Parameter;
 
 public class PrintMath {
     public static void main(String[] args) {
-        Class<?> aClass = Math.class;
-         Method[] methods =aClass.getDeclaredMethods();
-        for (Method method: methods) {
-            int modifiers = method.getModifiers();
-            if(Modifier.isPublic(modifiers)){
-//                out.append("public");
-            }
-            if (Modifier.isStatic(modifiers)){
-//                out.append("static");
+        Class<Math> aClass = Math.class;
+        Method[] methods = aClass.getDeclaredMethods();
+        Field[] fields = aClass.getDeclaredFields();
+        for (Field field : fields) {
+            if ((field.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC){
+                String contract = "public static " +
+                        field.getType() + " " + field.getName() + "(" +
+                        ")";
+                System.out.println(contract);
             }
         }
+
+        for (Method method : methods) {
+            if ((method.getModifiers() & Modifier.PUBLIC) == Modifier.PUBLIC){
+                StringBuilder contract = new StringBuilder();
+                contract.append("public static ");
+                Class<?> returnType = method.getReturnType();
+                String nameReturnType = returnType.getSimpleName();
+                contract.append(nameReturnType + " ").append(method.getName()).append("(");
+                Parameter[] parameters = method.getParameters();
+                String delimiter = "";
+                for (Parameter parameter : parameters) {
+                    contract.append(delimiter).append(parameter.getType().getSimpleName());
+                    delimiter = ",";
+                }
+                contract.append(")");
+                System.out.println(contract);
+            }
+        }
+
     }
 }
