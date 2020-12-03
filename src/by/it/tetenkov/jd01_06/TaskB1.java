73c0ba7f859b7f13b1107bd11077ba86dbbc7bcd
tetenkovg <43035829+tetenkovg@users.noreply.github.com>
2020-12-03 12:40:43 +0300

    Corrected jd01-06 B1

--- src/by/it/tetenkov/jd01_06/TaskB1.java
+++ src/by/it/tetenkov/jd01_06/TaskB1.java
@@ -8,11 +8,10 @@ import java.util.*;
 
 public class TaskB1 {
 
-
     private static String[] words = {};
-    private static String consonantsRussian = "цкнгшщзхфвпрлджбтмсч"; // соглаcные
+    private static String consonantsRussian = "цкнгшщзхфвпрлджбтмсчй"; // соглаcные
     private static String[] vowelsWords = {};
-    private static String vowelsRussian = "аоуэиыеёяюй"; // гласные
+    private static String vowelsRussian = "аоуэиыеёяю"; // гласные
 
 
     public static void main(String[] args) {
@@ -21,13 +20,10 @@ public class TaskB1 {
 
         while (matcher.find()) {
             String word = matcher.group();
-
             int last = words.length;
             words = Arrays.copyOf(words, last + 1);
             words[last] = word;
-
         }
-
         filter(words);
 
     }
@@ -35,18 +31,8 @@ public class TaskB1 {
     private static void filter(String[] words) {
         for (int i = 0; i < words.length; i++) {
             if (consonantsRussian.indexOf(Character.toLowerCase(words[i].charAt(0))) != -1 && vowelsRussian.indexOf(Character.toLowerCase(words[i].charAt(words[i].length() - 1))) != -1) {
-                String word = words[i];
-                int last = vowelsWords.length;
-                vowelsWords = Arrays.copyOf(vowelsWords, last + 1);
-                vowelsWords[last] = word;
-
+                System.out.println(words[i]);
             }
-
-        }
-
-        for (int i = 0; i < vowelsWords.length; i++) {
-            System.out.print(vowelsWords[i]);
-            System.out.println();
         }
     }
 }
